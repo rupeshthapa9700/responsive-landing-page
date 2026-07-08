fd0686815041641066d7772d9a14736a207b0bcd
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-08 22:03:50 +0545

    feat: complete Nexora landing page with JavaScript interactions

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
-
 // Open / Close menu
 menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+  navLinks.classList.toggle("active");
 
-    if(navLinks.classList.contains("active")){
-        menuBtn.textContent = "X";
-    }
-    else{
-        menuBtn.textContent = "☰";
-    }
+  if (navLinks.classList.contains("active")) {
+    menuBtn.textContent = "X";
+  } else {
+    menuBtn.textContent = "☰";
+  }
 });
-
 
 // Close menu when clicking a link
 const links = document.querySelectorAll(".nav-links a");
 
 links.forEach((link) => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
-        menuBtn.textContent = "☰";
-    });
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    menuBtn.textContent = "☰";
+  });
 });
 
-const startBauttons = document.querySelectorAll('.nexora-btn');
-    startBauttons.forEach((button) =>{
-        button.addEventListener("click", () =>{
-             alert("Thanks for choosing Nexora! 🚀");
-        });
-    });
+const startBauttons = document.querySelectorAll(".nexora-btn");
+startBauttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("Thanks for choosing Nexora! 🚀");
+  });
+});
+
+
+//FORM VALIDATION
+
+const contactForm = document.querySelector("#contactForm");
+
+
+contactForm.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const subject = document.querySelector("#subject").value;
+    const message = document.querySelector("#message").value;
+
+
+    if(name === "" || email === "" || subject === "" || message === ""){
+        alert("Please fill all fields!");
+    }
+    else{
+        alert("Message sent successfully! 🚀");
+        contactForm.reset();
+    }
+
+});
