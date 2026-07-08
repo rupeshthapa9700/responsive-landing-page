9370b7ce3071071b400f0f68d9407479c649a5b7
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-08 21:52:36 +0545

    feat: add button alerts and hamburger icon toggle

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const menuBtn = document.querySelector('.menu-btn');
-const navLinks = document.querySelector('.nav-links');
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
 
+
+// Open / Close menu
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+        menuBtn.textContent = "X";
+    }
+    else{
+        menuBtn.textContent = "☰";
+    }
 });
+
+
+// Close menu when clicking a link
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+        menuBtn.textContent = "☰";
+    });
+});
+
+const startBauttons = document.querySelectorAll('.nexora-btn');
+    startBauttons.forEach((button) =>{
+        button.addEventListener("click", () =>{
+             alert("Thanks for choosing Nexora! 🚀");
+        });
+    });
